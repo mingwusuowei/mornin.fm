a1e17729081e32f17a29e8a02b459a2a265e1dff
mingwusuowei <68714615+mingwusuowei@users.noreply.github.com>
2022-06-16 07:19:50 +0800

    Add files via upload

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,9 @@
-import en from './lang/en-US.json'
+import en from './lang/zh-CN.json'
 import zh from './lang/zh-CN.json'
-import zhTW from './lang/zh-TW.json'
-import ja from './lang/ja-JP.json'
-import es from './lang/es-ES.json'
-import de from './lang/de-DE.json'
+import zhTW from './lang/zh-CN.json'
+import ja from './lang/zh-CN.json'
+import es from './lang/zh-CN.json'
+import de from './lang/zh-CN.json'
 
 export default {
   fallbackLocale: 'en',
